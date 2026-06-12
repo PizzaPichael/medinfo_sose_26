84a5ccacca87a3907df0ae5fbf647d58100bfb30
PizzaPichael <michael.kaup@gmx.de>
2026-06-12 14:27:35 +0200

    feat: remove old test functions

--- src/fhir/fhir-client.js
+++ src/fhir/fhir-client.js
@@ -8,23 +8,6 @@ const runner = () => {
     console.log(`${hello}, ${where}`)
 }
 
-const searchPatients = async () => {
-    const result = await fetch('https://hapi.fhir.org/baseR4/Patient?_count=50000')
-    if (!result.ok) throw new Error(`HTTP ${result.status}`)
-    const patients = await result.json()
-    await fs.writeFile('patients.json', JSON.stringify(patients, undefined, 2))
-}
-
-const getPatient = async () => {
-    const result = await fetch('https://hapi.fhir.org/baseR4/Patient/90410791')
-    const patient = await result.json()
-    await fs.writeFile('patient.json', JSON.stringify(patient, undefined, 2))
-    console.log(patient)
-    console.log(patient.gender)
-    console.log(patient.birthDate)
-    console.log(patient.name[0])
-}
-
 const getPatientByAttribute = async (key, value) => {
     const params = newUrlSearchParams({ [key]: value })
     const result = await fetch('https://hapi.fhir.org/baseR4/Patient?${params}')
