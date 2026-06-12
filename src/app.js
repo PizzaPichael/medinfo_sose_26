1aea7cbbc0d77211eeaf1dac9b79babf0ebc9759
PizzaPichael <michael.kaup@gmx.de>
2026-06-12 14:26:11 +0200

    feat: add comment

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 const databse = require('./db/db')
 
 const main = async () => {
-    const url = 'mongodb://localhost:27017/test'
+    const url = 'mongodb://localhost:27017/patients'    
+    // Die Connection zu mongoDB wird über den Port sicher gestellt, weil mongo im Docker Container
+    // auf diese Portnummer horcht. Dieser ist gemapped auf den realen Port dieser Nummer. 
     const database = new DataBase(url)
     await database.connect()
-
     // Do stuff from here...
     
     //...to here.
