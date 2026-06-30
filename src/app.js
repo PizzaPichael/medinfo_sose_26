e9825d22614efb62c876f800022e193234dba2a2
Joh <132756126+JohPy@users.noreply.github.com>
2026-06-30 16:02:48 +0200

    feat: restructure app.js to include express setup and test endpoint

--- src/app.js
+++ src/app.js
@@ -1,23 +1,44 @@
 import DataBase from './db/db.js'
 import PatientSchema from './db/schemas/Patient.schema.js'
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.json())
+
+// can be extracted to a different file, currently just a test example
+const handleHelloWorld = (req, res) => {
+    console.log(req.params)
+    console.log(req.query)
+    console.log(req.body)
+
+    res.status(203).json({message: 'Hello World!'})
+}
 
 const main = async () => {
-    const url = 'mongodb://localhost:27017/patients'    
+    const url = 'mongodb://localhost:27017/patients'
     // Die Connection zu mongoDB wird über den Port sicher gestellt, weil mongo im Docker Container
     // auf diese Portnummer horcht. Dieser ist gemapped auf den realen Port dieser Nummer. 
     const database = new DataBase(url)
     await database.connect()
+    // api path definitions
+    app.get('/test', handleHelloWorld)
+
+    app.listen(port, () => {
+        console.log(`Server is running on http://localhost:${port}`)
+    })
+
     // Do stuff from here...
     const patientSchemaInstance = new PatientSchema({
         patientId: "3",
         id: "131896579",
         name: [
             {
-                use: "official", 
-                family: "Ramirez", 
+                use: "official",
+                family: "Ramirez",
                 given: [
                     "Carlos"
-                ] 
+                ]
             }
         ],
         gender: "male",
@@ -42,4 +63,8 @@ const main = async () => {
     await database.endConnection()
 }
 
-main().catch(e => console.log(e))
+try {
+    main()
+} catch (e) {
+    console.log(e)
+}
