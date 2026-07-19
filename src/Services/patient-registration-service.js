33a211f0d4e1418bd3fae0448cd2c3d4ecb6fcab
PizzaPichael <michael.kaup@gmx.de>
2026-07-19 18:00:02 +0200

    feat: moves all the registration logic relevante stuff to the registrationService and leaves the handler with the sole responsibility of receiving http requests, callign the right services and then sendign out responses dpendign on the outcome of the service

--- src/Services/patient-registration-service.js
+++ src/Services/patient-registration-service.js
@@ -1,3 +1,7 @@
+import AppError from '../errors/AppError.js'
+
+const tiebreaker = ['maritalStatus', 'address', 'telecom']
+
 class PatientRegistration {
 
     constructor(localDbClient, fhirClient) {
@@ -6,10 +10,92 @@ class PatientRegistration {
         console.log('[PATIENT-REGISTRATION] Created...')
     }
 
+    /**
+     * 
+     * @param {*} patientJson 
+     * @returns 
+     */
     registerPatient = async (patientJson) => {
-        const name = patientJson.name?.find(elem => elem.use == "official")
-        const birthDate = patientJson.birthDate
+        let localGetPatientResponse = null
+        try {
+            const official = patientJson.name.find(name => name.use == 'official')
+            localGetPatientResponse = await this.dataBaseClient.getPatientByFilter(
+                {
+                    'name.family': official.family,
+                    'name.given': official.given[0]
+                }
+            )
+        }
+        catch (e) {
+            throw e
+        }
 
-        Handler.askDbIfPatientExist(name, birthDate)
+        let wantedLocalPatientInstance
+        if(localGetPatientResponse != null) {
+            if(localGetPatientResponse.length > 1){
+                wantedLocalPatientInstance = tiebreak(localGetPatientResponse, patientJson)
+            } else {
+                wantedLocalPatientInstance = localGetPatientResponse[0]
+            }
+        }
+
+        else {
+            //TBD implement fhir client actions
+        }
+
+        return null //TBD
+    }
+
+    createPatient = async (patientJson) => {
+        //TBD remove or move to other service
+        try {
+            await this.dataBaseClient.addPatient(patientJson)
+            return true
+        }
+        catch (e) {
+            throw e
+        }
     }
 }
+
+/**
+ * Tiebreaker function, to filter out a unique patient, if several patients exist that share name and birthdate.
+ * @param {*} listOfPatientInstances    The list of non unique patients, sharing at least name and birthdate.
+ * @param {*} patientiToSearchFor       The patientJSON we got as the patient to register. 
+ * @param {*} i                         An iterator, that is increased while going through the lsit of patients. Default is 0.
+ * @returns                             One of the following: 
+ *                                      - A unique patient 
+ *                                      - Nothing in case no unique patient is found 
+ *                                      - A recursive call of itself
+ */
+const tiebreak = (listOfPatientInstances, patientiToSearchFor, i = 0) => {
+    console.log('[TIEBREAKER] Started')
+    if(listOfPatientInstances.length == 1) {
+        console.log('[TIEBREAKER] Unique patient found')
+        return listOfPatientInstances[0]
+    }
+    if(i >= tiebreaker.length) {
+        console.log('[TIEBREAKER] Ran out of tiebreakers. No unique patient found')
+        return null
+    }
+
+    const currentTiebreaker = tiebreaker[i]
+    console.log('[TIEBREAKER] Filtering by tiebreaker', currentTiebreaker)
+
+    // The next line is a function, that removes the mongoose '_id' value from an attribute of
+    // a patient instance, that mongoose has returned to us from the db.
+    // To make it comparable to the attribute of the JSON from the patient we need to register.
+    // Basicaly th function says "Turn this attribute and its content into a string except for the key '_id', that should be undefined."
+    // Which in this case means, it does not exist anymore.
+    const removeIdKeyFromMongoosePatientAttribute = (patientiInstance) => JSON.stringify(patientiInstance, (key, val) => key === '_id' ? undefined : val)
+
+    // The next three lines filter out all the patient instances, 
+    // that are equal to the to-be-registered-patient, regarding the tiebreaker.
+    // Only these instances are left and kept in the listOfPatientInstances.
+    const filteredlistOfPatientInstances = listOfPatientInstances.filter(
+        patient => removeIdKeyFromPatientInstance(patient[currentTiebreaker]) === JSON.stringify(patientiToSearchFor[currentTiebreaker])
+    )
+    return tiebreak(filteredlistOfPatientInstances, patientiToSearchFor, i + 1)
+}
+
+export default PatientRegistration
--- src/handler/handler.js
+++ src/handler/handler.js
@@ -1,14 +1,10 @@
-import DataBaseClient from "../db/db-client.js"
-
-const tiebreaker = ['maritalStatus', 'address', 'telecom']
-
 class Handler {
 
     /**
      * Constructor
-     * @param {*} localDbClient The dataBaseClient thats responsible for talking to the local DB
+     * @param {*} patientRegistrationService
      */
-    constructor(localDbClient, fhirClient, patientRegistrationService) {
+    constructor(patientRegistrationService) {
         this.patRegService = patientRegistrationService
         console.log('[HANDLER] Created...')
     }
@@ -23,94 +19,45 @@ class Handler {
     }
 
     /**
-     * Function to register a patient that shwos up at the frontdesk.
-     * @param {*} req API Endpoint input
-     * @param {*} res Endpoint response
+     * Function to pass registerPatient api call to the patienti-registration-service
+     * @param {*} req API Endpoint input object
+     * @param {*} res Endpoint response object
+     * TBD add returnvalues
      */
     registerPatient = async (req, res) => {
-        const patientiToRegisterJson = req.body //.patientJson // Der Input ist entsprechend des PatientSchema formatiert
-        let localGetPatientResponse = null
+        const patientToRegisterJson = req.body //.patientJson // Der Input ist entsprechend des PatientSchema formatiert
         try {
-            const official = patientiToRegisterJson.name.find(name => name.use == 'official')
-            localGetPatientResponse = await this.dataBaseClient.getPatientByFilter(
-                {
-                    'name.family': official.family,
-                    'name.given': official.given[0]
-                }
-            )
+            const wantedLocalPatientInstance = await this.patRegService.registerPatient(patientToRegisterJson)
+
+            res.status(200).json({ 
+                message: 'registerPatient request successfull',
+                'patientId':  wantedLocalPatientInstance.id
+            })
         }
         catch (e) {
             console.log('[HANDLER]: ', e)
             // Returns either the status code of the AppError Instance or defaults to a 500 status
             return res.status(e.statusCode ?? 500).json({ error: e.message })
         }
-        
-        let wantedLocalPatientInstance
-        if(localGetPatientResponse != null) {
-            if(localGetPatientResponse.length > 1){
-                wantedLocalPatientInstance = tiebreak(localGetPatientResponse, patientiToRegisterJson)
-            } else {
-                wantedLocalPatientInstance = localGetPatientResponse[0]
-            }
-        }
-        // console.log(wantedLocalPatientInstance)
-        
-        // !!!!---TBD hier: weiteren Verlauf deer Tranasktion hinzufügen, also Abfrage der FHIR, falls lokal nicht gefunden---!!!! //
-        
-        else {
-            
-        }
-
-        res.status(200).json({ 
-            message: 'registerPatient request accepted',
-            'patientId':  wantedLocalPatientInstance.id
-        })
     }
 
+    /**
+     * Triggers creation of the patient in the registrationService
+     * @param {*} req API Endpoint input object
+     * @param {*} res Endpoint response object
+     * TBD add returnvalues
+     */
     createPatient = async (req, res) => {
-        await this.dataBaseClient.addPatient(req.body)
-        res.status(200).json({message: 'Patient successfully created'})
+        //TBD remove or move to other service
+        try {
+            const patientCreated = await this.patRegService.addPatient(req.body)
+            res.status(200).json({message: 'Patient successfully created'})
+        }
+        catch (e) {
+            console.log('[HANDLER]: ', e)
+            return res.status(e.statusCode ?? 500).json({ error: e.message })
+        }
     }
-}
-
-/**
- * Tiebreaker function, to filter out a unique patient, if several patients exist that share name and birthdate.
- * @param {*} listOfPatientInstances    The list of non unique patients, sharing at least name and birthdate.
- * @param {*} patientiToSearchFor       The patientJSON we got as the patient to register. 
- * @param {*} i                         An iterator, that is increased while going through the lsit of patients. Default is 0.
- * @returns                             One of the following: 
- *                                      - A unique patient 
- *                                      - Nothing in case no unique patient is found 
- *                                      - A recursive call of itself
- */
-const tiebreak = (listOfPatientInstances, patientiToSearchFor, i = 0) => {
-    console.log('[TIEBREAKER] Started')
-    if(listOfPatientInstances.length == 1) {
-        console.log('[TIEBREAKER] Unique patient found')
-        return listOfPatientInstances[0]
-    }
-    if(i >= tiebreaker.length) {
-        console.log('[TIEBREAKER] Ran out of tiebreakers. No unique patient found')
-        return null
-    }
-
-    const currentTiebreaker = tiebreaker[i]
-    console.log('[TIEBREAKER] Filtering by tiebreaker', currentTiebreaker)
-
-    // The next line is a function, that removes the mongoose '_id' value from an attribute of
-    // a patient instance, that mongoose has returned to us from the db.
-    // To make it comparable to the attribute of the JSON from the patient we need to register.
-    // Basicaly th function says "Turn this attribute and its content into a string except for the key '_id', that should be undefined."
-    // Which in this case means, it does not exist anymore.
-    const removeIdKeyFromMongoosePatientAttribute = (patientiInstance) => JSON.stringify(patientiInstance, (key, val) => key === '_id' ? undefined : val)
-
-    // The next three lines filter out all the patient instances, 
-    // that are equal to the to-be-registered-patient, regarding the tiebreaker.
-    // Only these instances are left and kept in the listOfPatientInstances.
-    const filteredlistOfPatientInstances = listOfPatientInstances.filter(
-        patient => removeIdKeyFromPatientInstance(patient[currentTiebreaker]) === JSON.stringify(patientiToSearchFor[currentTiebreaker])
-    )
-    return tiebreak(filteredlistOfPatientInstances, patientiToSearchFor, i + 1)
 }
 
 export default Handler
